1800ee7abc577c80f4929994941befc481e7a418
Teri11 <augustineteri157@gmail.com>
2022-04-27 16:31:59 +0300

    initial commit

--- worklinks/src/app/app.module.ts
+++ worklinks/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationBarComponent
+    NavigationBarComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
