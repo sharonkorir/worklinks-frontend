e46abe9a4aaf93144faf8f147c029d8ca2893a64
Venus-Kavunza <venusmwende@gmail.com>
2022-04-28 15:14:26 +0300

    Added bootstrap link

---
+++ worklinks/src/app/register-jobseeker/register-jobseeker.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-jobseeker',
+  templateUrl: './register-jobseeker.component.html',
+  styleUrls: ['./register-jobseeker.component.css']
+})
+export class RegisterJobseekerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
