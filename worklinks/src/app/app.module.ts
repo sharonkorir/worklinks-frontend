f8ccd1fd337a2aa060126386aff44f91357b6c10
sharonkorir <sharon.korir@student.moringaschool.com>
2022-04-28 15:24:08 +0300

    create employer sign up component

--- worklinks/src/app/app.module.ts
+++ worklinks/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployerSignupComponent } from './employer-signup/employer-signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployerSignupComponent
   ],
   imports: [
     BrowserModule,
