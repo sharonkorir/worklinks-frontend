e0bc7b04ac25e9182d95a3f5dce051872823bead
sharonkorir <sharon.korir@student.moringaschool.com>
2022-04-28 19:22:07 +0300

    create employer sign in form

--- worklinks/src/app/employer-login/employer-login.component.ts
+++ worklinks/src/app/employer-login/employer-login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-employer-login',
@@ -6,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employer-login.component.css']
 })
 export class EmployerLoginComponent implements OnInit {
+
+  email = new FormControl('');
+  name = new FormControl('');
+  phone = new FormControl('');
+  password1 = new FormControl('');
+  password2 = new FormControl('');
 
   constructor() { }
 
