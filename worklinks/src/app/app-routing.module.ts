cafc1d19f06158f4555652416c37eb401fbb7663
sharonkorir <sharon.korir@student.moringaschool.com>
2022-04-29 02:10:14 +0300

    routing: create login and register paths for employer

--- worklinks/src/app/app-routing.module.ts
+++ worklinks/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployerSignupComponent } from './employer-signup/employer-signup.component';
+import { EmployerLoginComponent } from './employer-login/employer-login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'register', component: EmployerSignupComponent},
+  { path: 'login', component: EmployerLoginComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
