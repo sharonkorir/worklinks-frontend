363cb56cfb9f3b1cea78cb3f98dd71cf3bcfa776
saretotracy <tracysareto@gmail.com>
2022-04-29 11:36:25 +0300

    created recruiter & profile routing

--- worklinks/src/app/app-routing.module.ts
+++ worklinks/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { ProfileComponent } from './profile/profile.component';
+import { RecruiterProfileComponent } from './recruiter-profile/recruiter-profile.component';
+const routes: Routes = [
+  {
+    path:'',component:ProfileComponent
+  },
+  {
+    path:'recruiter-profile',component:RecruiterProfileComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
+ 
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
