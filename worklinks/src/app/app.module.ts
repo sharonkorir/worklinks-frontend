dd6f596d644628e55a4ce65912c08ad3202657e3
sharonkorir <sharon.korir@student.moringaschool.com>
2022-04-30 10:12:49 +0300

    fix bootstrap 5 configuration to display mpesa modal

--- worklinks/src/app/app.module.ts
+++ worklinks/src/app/app.module.ts
@@ -1,24 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatDialogModule} from '@angular/material/dialog';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployerSignupComponent } from './employer-signup/employer-signup.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { EmployerLoginComponent } from './employer-login/employer-login.component';
 import { EmployerNavbarComponent } from './employer-navbar/employer-navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployerSignupComponent,
     EmployerLoginComponent,
-    EmployerNavbarComponent
+    EmployerNavbarComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
