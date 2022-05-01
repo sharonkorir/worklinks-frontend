b9f636edb3abf9fde89075dd0dd7bc4b28e991fc
Venus-Kavunza <venusmwende@gmail.com>
2022-05-02 00:02:26 +0300

    Added applicants page

--- worklinks/src/app/app.module.ts
+++ worklinks/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterJobseekerComponent } from './register-jobseeker/register-jobseeker.component';
+import { LoginJobseekerComponent } from './login-jobseeker/login-jobseeker.component';
+import { ApplicantsComponent } from './applicants/applicants.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterJobseekerComponent
+    RegisterJobseekerComponent,
+    LoginJobseekerComponent,
+    ApplicantsComponent
   ],
   imports: [
     BrowserModule,
