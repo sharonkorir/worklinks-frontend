b92179e16d06a181508bf2a0d13c171ac6ed3f58
sharonkorir <sharon.korir@student.moringaschool.com>
2022-05-03 12:37:38 +0300

    display navbar in search component

--- worklinks/src/app/app-routing.module.ts
+++ worklinks/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmployerSignupComponent } from './employer-signup/employer-signup.component';
 import { EmployerLoginComponent } from './employer-login/employer-login.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
 
 const routes: Routes = [
   { path: 'employer/register', component: EmployerSignupComponent},
   { path: 'employer/login', component: EmployerLoginComponent},
+  { path: 'search', component: SearchResultsComponent},
 ];
 
 @NgModule({
