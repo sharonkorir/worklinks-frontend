ad529b0555f37066364b182bf1cf8e9d669312ac
saretotracy <tracysareto@gmail.com>
2022-05-03 22:28:41 +0300

    add promises function to post-job component to post data to the api

--- worklinks/src/app/app.module.ts
+++ worklinks/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import {HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
@@ -12,7 +12,7 @@ import { RecruiterNavbarComponent } from './recruiter-navbar/recruiter-navbar.co
 import { RecruiterDashboardComponent } from './recruiter-dashboard/recruiter-dashboard.component';
 import { PostJobFormComponent } from './post-job-form/post-job-form.component';
 import { JobseekerProfilePageComponent } from './jobseeker-profile-page/jobseeker-profile-page.component';
-
+import { FormBuilder } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -30,9 +30,12 @@ import { JobseekerProfilePageComponent } from './jobseeker-profile-page/jobseeke
     HttpClientModule,
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    FormBuilder,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- worklinks/src/app/post-job-form/post-job-form.component.ts
+++ worklinks/src/app/post-job-form/post-job-form.component.ts
@@ -1,15 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-post-job-form',
   templateUrl: './post-job-form.component.html',
   styleUrls: ['./post-job-form.component.css']
 })
 export class PostJobFormComponent implements OnInit {
+  form!:FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder:FormBuilder,
+    private http:HttpClient,
+    private router:Router,
+  ) { }
 
   ngOnInit(): void {
+    this.form =this.formBuilder.group({
+      title:'',
+      requirements:'',
+      location:'',
+      jobtype:'',
+      
+    })
   }
-
+submit():void{
+  console.log(this.form.getRawValue());
+  this.http.post('http://127.0.0.1:8000/api/Job/',this.form.getRawValue())
+  subscribe(() => this.router.navigate(['recruiter-dashboard']));
+}
+}
+function subscribe(arg0: () => Promise<boolean>) {
+  throw new Error('Function not implemented.');
 }
