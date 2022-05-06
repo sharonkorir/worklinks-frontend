0fc5e42f2b3a796850286f4e771f46179d20e377
saretotracy <tracysareto@gmail.com>
2022-05-06 11:01:38 +0300

    add changes to reqruiter profile

--- worklinks/src/app/recruiter-dashboard/recruiter-dashboard.component.ts
+++ worklinks/src/app/recruiter-dashboard/recruiter-dashboard.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import * as Emitter from 'component-emitter';
 import TypedEmitter from "typed-emitter";
 import { SearchFilterPipe } from '../search-filter.pipe';
@@ -23,23 +23,48 @@ export class Job{
 })
 export class RecruiterDashboardComponent implements OnInit {
  public searchFilter: any = '';
-
+ deleteId!: number;
  jobs!: Job[];
+  dialog: any;
+  status!: string;
     
   constructor(
     private http:HttpClient
   ) { }
 
+
+
   ngOnInit(): void {
  this.getJobs();
+ this.deletejobs();
   }
+  deletejobs(){
+    this.http.delete('http://127.0.0.1:8000/api/Job/')
+            .subscribe(() => this.status = 'Delete successful')
+  }
+  
   getJobs(){
-    this.http.get<any>('http://127.0.0.1:8000/api/Job/').subscribe(
+    this.http.get<any>('http://worklinks.herokuapp.com/api/Job/').subscribe(
       response => {
         console.log(response);
         this.jobs = response;
       }
     );
+  }
+  onDelete() {
+    const deleteURL = 'http://127.0.0.1:8000/api/Job/' + this.deleteId + '/delete';
+    this.http.delete(deleteURL)
+      .subscribe((results) => {
+        this.ngOnInit();
+        this.dialog.closeAll();
+      });
+  }
+  openDelete(templateRef:TemplateRef<any>, job: any) {
+    this.deleteId = job.id;
+    this.dialog.open(templateRef, {
+      with:'40%',
+      height:'70%'
+    });
   }
 
 }
