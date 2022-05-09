1077b42591899e41aaeda31a73092c4eb736b096
sharonkorir <sharon.korir@student.moringaschool.com>
2022-05-09 23:25:21 +0300

    save navbar and routing updates

--- worklinks/src/app/jobseeker-profile-page/jobseeker-profile-page.component.ts
+++ worklinks/src/app/jobseeker-profile-page/jobseeker-profile-page.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+export class Profile{
+  constructor(
+    public id: number,
+    public full_name:string,
+    public address: string,
+    public email: string,
+    public location: string,
+    public jobtype: string,
+    public profile_image:any,
+    public resume:any,
+    public bio:string,
+    public work_experience:string,
+    public skills:string,
+    public referees:string,
+
+   
+  ) {
+  }
+}
 @Component({
   selector: 'app-jobseeker-profile-page',
   templateUrl: './jobseeker-profile-page.component.html',
   styleUrls: ['./jobseeker-profile-page.component.css']
 })
 export class JobseekerProfilePageComponent implements OnInit {
+profile!:Profile[];
 
-  constructor() { }
+  constructor( 
+    private http:HttpClient) { }
 
   ngOnInit(): void {
+    this.getprofile();
   }
 
+  getprofile(){
+    this.http.get<any>('https://moiwork.herokuapp.com/api/UpdateUserProfile/').subscribe(
+      response => {
+        console.log(response);
+        this.profile = response;
+      }
+    );
+  }
 }
