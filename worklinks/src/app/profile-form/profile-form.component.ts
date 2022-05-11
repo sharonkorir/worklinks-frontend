07eb37cca58f7f061d1afb9473d3bd0668fdfd40
saretotracy <tracysareto@gmail.com>
2022-05-11 03:35:57 +0300

    merge registration

--- worklinks/src/app/profile-form/profile-form.component.ts
+++ worklinks/src/app/profile-form/profile-form.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { FormBuilder, FormGroup } from '@angular/forms';
@@ -13,16 +12,6 @@ import { Emitters } from '../emmiters/emmiters';
   styleUrls: ['./profile-form.component.css']
 })
 export class ProfileFormComponent implements OnInit {
-<<<<<<< HEAD
-
-  data: any;
-  constructor(private http:HttpClient) { }
-  onSubmit(data: any){
-    this.http.post('https://moiwork.herokuapp.com/api/UpdateUserProfile/',data)
-    .subscribe((results) =>{
-      console.warn('results',results)
-      alert("Profile updated Successfully")
-=======
   form!:FormGroup;
   message = '';
   constructor(
@@ -30,13 +19,7 @@ export class ProfileFormComponent implements OnInit {
     private http:HttpClient,
     private router:Router,
   ) { }
->>>>>>> registration
 
-      this.data.reset();
-    })
-    console.log(data);
-    
-     }
   ngOnInit(): void {
     this.http.get('https://moiwork.herokuapp.com/user', {withCredentials: true}).subscribe(
       (res: any) => {
